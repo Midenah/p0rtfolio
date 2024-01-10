aad6049431e05aa99c79da61dc40b4edcffd0a3e
oluyomi justina <justinaolamide2611@gmail.com>
2024-01-10 15:47:45 +0100

    fix

--- my-project/next.config.js
+++ my-project/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig;
-module.exports = {
-    // other exports
-    target: 'serverless', // add this line
-  };
+module.exports = nextConfig
+
